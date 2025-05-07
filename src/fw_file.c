079d7c20f88bfaf0124579e30b34e39aa4dcce23
Michael Heimpold <michael.heimpold@chargebyte.com>
2025-05-07 13:03:56 +0200

    Extend firmware info with platform and application type
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

--- src/fw_file.c
+++ src/fw_file.c
@@ -17,6 +17,36 @@
 #include <tools.h>
 #include "fw_file.h"
 
+const char *fw_sw_platform_type_to_str(uint8_t type)
+{
+    switch (type) {
+    case SW_PLATFORM_TYPE_UNSPECIFIED:
+        return "unspecified";
+    case SW_PLATFORM_TYPE_UNKOWN:
+        return "unknown";
+    case SW_PLATFORM_TYPE_DEFAULT:
+        return "default";
+    case SW_PLATFORM_TYPE_CCY:
+        return "CCY";
+    default:
+        return "unknown value";
+    }
+}
+
+const char *fw_sw_application_type_to_str(uint8_t type)
+{
+    switch (type) {
+    case SW_APPLICATION_TYPE_FIRMWARE:
+        return "firmware";
+    case SW_APPLICATION_TYPE_EOL:
+        return "eol";
+    case SW_APPLICATION_TYPE_QUALI:
+        return "qualification";
+    default:
+        return "unknown";
+    }
+}
+
 int fw_mmap(const char *filename, uint8_t **content, unsigned long *filesize)
 {
     struct stat fileinfo;
@@ -80,6 +110,11 @@ void fw_dump_version_app_infoblock(struct version_app_infoblock *p)
 
     printf("Firmware Version:          %" PRIu8 ".%" PRIu8 ".%" PRIu8 "\n",
            p->sw_major_version, p->sw_minor_version, p->sw_build_version);
+    printf("Firmware Platform Type:    %s (0x%02" PRIx8 ")\n",
+           fw_sw_platform_type_to_str(p->sw_platform_type), p->sw_platform_type);
+    printf("Firmware Application Type: %s (0x%02" PRIx8 ")\n",
+            fw_sw_application_type_to_str(p->sw_application_type), p->sw_application_type);
+
     printf("Git Hash:                  %016" PRIx64 "\n", p->git_hash);
 
     printf("End Magic Pattern:         0x%08" PRIx32 "\n", p->end_magic_pattern);
@@ -93,12 +128,14 @@ bool fw_print_amended_version_app_infoblock(struct version_app_infoblock *p, con
      * Firmware Size:             23248
      * Firmware Checksum (CRC32): 0xb6ca0819
      * Firmware Version:          0.1.0
+     * Firmware Platform Type:    default (0x81)
+     * Firmware Application Type: qualification (0x05)
      * Git Hash:                  a9653ba5c34eeba8
      * End Magic Pattern:         0xcafebabe
      *
      * We enclose it a little bit to make it nice.
      */
-    const char *padding = "===========================================";
+    const char *padding = "===============================================";
     bool is_valid = fw_valid_version_app_infoblock(p);
     int padding_length = strlen(padding) - 6 - strlen(header);
 
