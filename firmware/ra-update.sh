dd437e56852af6dbec9e6d98b364f390d2227140
Michael Heimpold <michael.heimpold@chargebyte.com>
2025-06-18 16:09:49 +0200

    Add firmware update script and systemd helper
    The script is designed to run during boot, i.e. before the EVerest
    system is started because it requires UART access.
    It just compares the firmware included in the root filesystem
    with the flashed version and overwrites it if needed.
    
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

---
+++ firmware/ra-update.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# This script checks whether the Safety Controller needs an update.
+# For this, it checks the current flashed firmware information and compares
+# it with the data from the firmware bundle in the rootfs.
+# If this differs, then the firmware is updated.
+#
+
+LIBDIR="/usr/share/csom-safety-fw"
+
+FW_FILE="$(ls -1 $LIBDIR/*_fw_*.bin)"
+PARAM_FILE="$(ls -1 $LIBDIR/*_parameter-block_only-contactor.bin)"
+
+TARGET_VERSION="$(ra-update fw_info "$FW_FILE")"
+CURRENT_VERSION="$(ra-update fw_info)"
+
+# do nothing if we cannot detect current or target version
+[ -z "$CURRENT_VERSION$TARGET_VERSION" ] && exit 1
+
+CMP_TARGET_VERSION="$(echo "$TARGET_VERSION" | tail -n +3 | head -n 6)"
+CMP_CURRENT_VERSION="$(echo "$CURRENT_VERSION" | tail -n +3 | head -n 6)"
+
+# nothing to do when versions are equal
+[ "$CMP_TARGET_VERSION" = "$CMP_CURRENT_VERSION" ] && exit 0
+
+cat <<EOF
+== Safety Controller Firmware Update required ==
+
+<<< Current Firmware <<<
+$CMP_CURRENT_VERSION
+<<<
+
+>>> New Firmware >>>
+$CMP_TARGET_VERSION
+>>>
+
+================================================
+EOF
+
+CURRENT_VERSION_ONLY="$(echo "$CMP_CURRENT_VERSION" | grep "Version" | awk '{print $3}')"
+
+# when upgrading from 0.1.0 we need to install a parameter block for the first time;
+# later we assume that a valid parameter block is already installed and we don't touch it
+if [ "$CURRENT_VERSION_ONLY" = "0.1.0" ]; then
+    echo -n "Updating Parameter Block..."
+    ra-update -a data flash "$PARAM_FILE"
+    echo "done."
+fi
+
+echo -n "Updating Firmware..."
+ra-update flash "$FW_FILE"
+rv=$?
+echo "done."
+
+exit "$rv"
