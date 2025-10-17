6e3441008be3dd700847425a4aa0038130f6954e
Michael Heimpold <michael.heimpold@chargebyte.com>
2025-10-17 12:06:29 +0200

    Adapt firmware update script to handle new version
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

--- firmware/ra-update.sh
+++ firmware/ra-update.sh
@@ -41,9 +41,44 @@ EOF
 
 CURRENT_VERSION_ONLY="$(echo "$CMP_CURRENT_VERSION" | grep "Version" | awk '{print $3}')"
 
+version_cmp() {
+    local v1="$1"
+    local v2="$2"
+    local smaller_version="$(printf "%s\n%s" "$v1" "$v2" | sort -V | head -n1)"
+
+    if [ "$smaller_version" = "$v1" ] && [ "$v1" != "$v2" ]; then
+        echo "-1"
+    elif [ "$v1" = "$v2" ]; then
+        echo "0"
+    else
+        echo "1"
+    fi
+}
+
+# lesser than
+cur_version_lt() {
+    [ "$(version_cmp "$CURRENT_VERSION_ONLY" "$1")" = "-1" ]
+}
+
+# lesser than or equal
+cur_version_le() {
+    local rv="$(version_cmp "$CURRENT_VERSION_ONLY" "$1")"
+    [ "$rv" = "-1" ] || [ "$rv" = "0" ]
+}
+
+# greater than
+cur_version_gt() {
+    [ "$(version_cmp "$CURRENT_VERSION_ONLY" "$1")" = "1" ]
+}
+
+# equal
+cur_version_eq() {
+    [ "$CURRENT_VERSION_ONLY" = "$1" ]
+}
+
 # when upgrading from 0.1.0 we need to install a parameter block for the first time;
 # later we assume that a valid parameter block is already installed and we don't touch it
-if [ "$CURRENT_VERSION_ONLY" = "0.1.0" ]; then
+if cur_version_eq "0.1.0"; then
     pbfile_target_bin=$(mktemp)
 
     ra-pb-create -i "$PARAM_FILE" -o "$pbfile_target_bin"
@@ -55,9 +90,10 @@ if [ "$CURRENT_VERSION_ONLY" = "0.1.0" ]; then
     echo "done."
 fi
 
-# when upgrading from 0.2.2 we need to update the parameter block since the magic value
-# for disabled PT1000 channels changed
-if [ "$CURRENT_VERSION_ONLY" = "0.2.2" ]; then
+# when upgrading from 0.2.2 or before we need to update the parameter block since
+# the magic value for disabled PT1000 channels changed; the check for >= 0.1.0
+# is just a short-cut since in this case, we already updated with the install above
+if cur_version_gt "0.1.0" && cur_version_le "0.2.2"; then
     echo "Updating Parameter Block..."
 
     pbfile_current_bin=$(mktemp)
@@ -74,6 +110,33 @@ if [ "$CURRENT_VERSION_ONLY" = "0.2.2" ]; then
 
     # re-create parameter block - while creating YAML, the old magic values was already handled
     # so we can just re-create the new binary file using the dumped YAML file
+    ra-pb-create -i "$pbfile_current_yaml" -o "$pbfile_target_bin"
+
+    # finally flash it
+    ra-update -a data flash "$pbfile_target_bin"
+
+    rm -f "$pbfile_current_bin" "$pbfile_current_yaml" "$pbfile_target_bin"
+    echo "done."
+fi
+
+# firmware < 0.2.6 had no versioned parameter file, ra-pb-dump can handle this
+# but we need to write it back
+if cur_version_lt "0.2.6"; then
+    echo "Updating Parameter Block..."
+
+    pbfile_current_bin=$(mktemp)
+    pbfile_current_yaml=$(mktemp)
+    pbfile_target_bin=$(mktemp)
+
+    # dump current parameter block to YAML file
+    ra-update -a data dump "$pbfile_current_bin"
+    ra-pb-dump "$pbfile_current_bin" > "$pbfile_current_yaml"
+
+    # dump it indented to stdout (for debug purpose only)
+    echo "Current parameters:"
+    cat "$pbfile_current_yaml" | sed 's/^/    /'
+
+    # re-create parameter block
     ra-pb-create -i "$pbfile_current_yaml" -o "$pbfile_target_bin"
 
     # finally flash it
